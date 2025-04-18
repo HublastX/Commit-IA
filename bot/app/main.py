c14c717d74a36d680161f2573696f5aafa837a7d
wendellast <wendelalves01a@hotmail.com>
2025-04-18 17:27:13 -0300

    fix(bot): corrige título da api para commit-ia

--- bot/app/main.py
+++ bot/app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from routes.router import register_routers
 
-app = FastAPI(title="Eduga API", root_path="/commit-ia")
+app = FastAPI(title="Commit-IA", root_path="/commit-ia")
 
 
 register_routers(app)
