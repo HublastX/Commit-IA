e90f4d617b1a2eeb8bef6144d877cab4d90e2ccd
wendellast <wendelalves01a@hotmail.com>
2025-04-18 17:00:29 -0300

    fix(services): correção do erro path não localizado

--- cmd/services/configPath/getFilePath.go
+++ cmd/services/configPath/getFilePath.go
@@ -4,10 +4,19 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"runtime"
 )
 
 func getConfigFilePath() (string, error) {
-	configDir := filepath.Join(".", "bot", "app", "config")
+
+	_, currentFile, _, ok := runtime.Caller(0)
+	if !ok {
+		return "", fmt.Errorf("error getting current file path")
+	}
+
+	baseDir := filepath.Dir(filepath.Dir(filepath.Dir(filepath.Dir(currentFile))))
+
+	configDir := filepath.Join(baseDir, "bot", "app", "config")
 
 	if err := os.MkdirAll(configDir, 0755); err != nil {
 		return "", fmt.Errorf("error creating config directory: %v", err)
