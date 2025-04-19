5a88face032b9d4221a491ea47011ba61b40acd7
wendellast <wendelalves01a@hotmail.com>
2025-04-18 21:53:03 -0300

    refactor(bot_models.py): externaliza leitura de configuração e ajusta mensagens de erro

--- bot/app/services/bot_models.py
+++ bot/app/services/bot_models.py
@@ -1,26 +1,8 @@
-import json
-import os
-from typing import Dict
 
 from google import genai
 from openai import OpenAI
 from services.llm_base import LLMBase
-
-
-def read_commitia_config() -> Dict:
-    """
-    Read the commitia config file from config/config.json (dentro do /app no Docker).
-    """
-
-    config_path = os.path.join(os.path.dirname(__file__), "../config/config.json")
-    config_path = os.path.abspath(config_path)
-    if not os.path.exists(config_path):
-        raise FileNotFoundError(
-            f"Configuração não encontrada em {config_path}. Por favor, execute 'commitia --update' para configurar."
-        )
-    with open(config_path, 'r') as f:
-        config = json.load(f)
-    return config
+from tools.read_commit_config import read_commitia_config
 
 try:
     config = read_commitia_config()
@@ -28,33 +10,34 @@ except FileNotFoundError as e:
     print(e)
     config = None
 
+
 client = None
+
+
 if config:
     if config.get("provider") == "google" and config.get("api_key"):
         client = genai.Client(api_key=config.get("api_key"))
     elif config.get("provider") == "openai" and config.get("api_key"):
         client = OpenAI(api_key=config.get("api_key"))
 
+
 def botModel(provider=None, model=None, api_key=None):
     if not config:
-        raise ValueError("Configuração não encontrada. Por favor, execute 'commitia --update' para configurar.")
+        raise ValueError("Configuration not found. Please run 'commitia --update' to configure.")
 
     provider = provider or config.get("provider")
     if not provider:
-        raise ValueError("LLM provider não especificado na configuração.")
+        raise ValueError("LLM provider not specified in configuration.")
 
     model = model or config.get("model")
     if not model:
-        raise ValueError(f"LLM model não especificado para o provider '{provider}' na configuração.")
+        raise ValueError(f"LLM model not specified for provider '{provider}' in configuration.")
 
     api_key = api_key or config.get("api_key")
-
-
-
     if not api_key:
-        raise ValueError(f"API key não encontrada para o provider '{provider}' na configuração.")
+        raise ValueError(f"API key not found for provider '{provider}' in configuration.")
 
-    print(f"Usando provider: {provider}, model: {model}")
+#    print(f"Using provider: {provider}, model: {model}")
 
     modelo_base = LLMBase(
         provider=provider,
@@ -63,5 +46,3 @@ def botModel(provider=None, model=None, api_key=None):
         api_key=api_key
     )
     return modelo_base
-
-
