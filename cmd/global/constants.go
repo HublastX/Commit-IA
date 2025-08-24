fac238bc9ce9f67621da9f9daf8debb38a19f121
wendellast <wendelalves01a@hotmail.com>
2025-08-24 19:31:05 -0300

    chore(cmd/global): atualiza url remota padrão

--- cmd/global/constants.go
+++ cmd/global/constants.go
@@ -2,5 +2,5 @@ package global
 
 const (
 	DefaultLocalURL  = "http://127.0.0.1:6013/commit-analyzer/"
-	DefaultRemoteURL = "http://localhost:8080/llm"
+	DefaultRemoteURL = "https://hublast.com/commit-ai-hub/llm"
 )
